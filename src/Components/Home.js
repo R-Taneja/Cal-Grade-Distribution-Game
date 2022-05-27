f9eef7f1e649710c122992999fb70e3b0c702d69
Rohan Taneja <47066511+R-Taneja@users.noreply.github.com>
2022-05-26 21:59:16 -0400

    Fixed typo

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ function Home() {
   const { setGameState } = useContext(QuizContext);
   return (
     <div className="ms-display-flex ms-flex-direction-column ms-flex-justify-center ms-flex-align-items-center ms-flex-align-content-center">
-        <button className='ms-btn ms-primary ms-large ms-box-shadow' onClick={() => {setGameState("quiz")}}>Start</button>
+        <button className='ms-btn ms-primary ms-large ms-box-shadow' onClick={() => {setGameState("quiz")}}>START</button>
     </div>
   )
 }
